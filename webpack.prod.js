5578d248ccb1c0d8056b26bbd149e8a4b4104dca
threegeese <19970913qjh@gmail.com>
2019-06-17 15:02:59 +0800

    yasu

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@
 
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -16,8 +18,14 @@ module.exports = {
     module: {
         rules: [
             { test: /.js$/, use: "babel-loader" },
-            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
-            { test: /\.less$/, use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"] },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.less$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
+            },
             {
                 test: /\.(jpg|png|gif|jpeg)$/,
                 use: {
@@ -40,7 +48,39 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '[name]_[contenthash:8].css'
+            filename: "[name]_[contenthash:8].css"
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require("cssnano")
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "./src/index.html"),
+            filename: "index.html",
+            chunks: ["index"],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "./src/search.html"),
+            filename: "search.html",
+            chunks: ["search"],
+            inject: true,
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyCSS: true,
+                minifyJS: true,
+                removeComments: false
+            }
         })
     ]
 };
